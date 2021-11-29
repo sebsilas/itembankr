75759f67f6740c5fcfd09c5028adeee6a96992fa
syntheso <sebsilas@gmail.com>
2021-11-29 13:49:57 +0000

    MAST21 updates
    Former-commit-id: 21170ac529c807251bf34082923c3db75f425de1
    Former-commit-id: e4124740876cc1e852c61f87189c0833752d9f00

--- R/item_bank_explorer.R
+++ R/item_bank_explorer.R
@@ -67,11 +67,7 @@ item_bank_explorer <- function(item_bank) {
 
   ui <- shiny::basicPage(
 
-    musicassessr::musicassessr_js_scripts(api_url = "https://255uxe6ajl.execute-api.us-east-1.amazonaws.com/api",
-                                          bucket_name = "shinny-app-source-41630",
-                                          bucket_region = "us-east-1",
-                                          identity_pool_id = "us-east-1:feecdf7e-cdf6-416f-94d0-a6de428c8c6b",
-                                          destination_bucket = "shinny-app-destination-41630"),
+    musicassessr::musicassessr_js_scripts(),
 
     shiny::tags$h2("Corpus Explorer"),
 
